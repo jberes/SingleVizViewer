383e379af30d9965f9f1e1a3499f2f36b3133449
Jason Beres <jasonberes@gmail.com>
2024-10-14 16:10:49 -0400

    Udpated w/ latest WC

--- src/app/master-view/master-view.component.ts
+++ src/app/master-view/master-view.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { RevealSdkSettings, RevealViewOptions } from '@revealbi/ui';
+import { RevealSdkSettings } from '@revealbi/ui';
 import { Subject, takeUntil } from 'rxjs';
 import { VisualizationChartInfo } from '../models/reveal-server/visualization-chart-info';
 import { RevealServerService } from '../services/reveal-server.service';
@@ -9,15 +9,10 @@ import { RevealServerService } from '../services/reveal-server.service';
   templateUrl: './master-view.component.html',
   styleUrls: ['./master-view.component.scss']
 })
+
 export class MasterViewComponent implements OnInit, OnDestroy {
-  dashboardOptions: RevealViewOptions = {
-    visualizations: {
-      menu: {
-        copy: false,
-        duplicate: false
-      }
-    }
-  };
+  _singleViz?: string;
+  _singleVizDocument?: string;
   private destroy$: Subject<void> = new Subject<void>();
   public vizInfo?: VisualizationChartInfo;
   public revealServer: string = 'https://reveal-api.azurewebsites.net/';
@@ -41,6 +36,9 @@ export class MasterViewComponent implements OnInit, OnDestroy {
   }
 
   public listItemClick(item: VisualizationChartInfo) {
+    console.log('listItemClick', item);
     this.vizInfo = item;
+    this._singleVizDocument = item.dashboardFileName;
+    this._singleViz = item.vizId;
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,9 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
+import { defineRevealSdkWrappers } from "reveal-sdk-wrappers";
+defineRevealSdkWrappers();
+
 if (environment.production) {
   enableProdMode();
 }
